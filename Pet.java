58a1a13201f609b8e3a1b91995eddc159b029620
Student: Aya Aya <94848837+ayamuthanna@users.noreply.github.com>
2022-06-13 13:18:39 +0000

    Initial commit

---
+++ Pet.java
@@ -0,0 +1,50 @@
+public class Pet
+{
+// instance data
+private double weight;
+private String species;
+private int age;
+private boolean pureBred;
+
+// constructors
+  public Pet( String s, double w, int a, boolean pb  )
+  {
+    setName(n);
+    setWeight(w);
+    setAge(a);
+    setBreed(pb);
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
